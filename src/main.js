cbe554f4e105a0141a6373452e7d7814f66bdde2
alpachm <alexpacheco407@gmail.com>
2022-12-31 17:52:31 -0500

    menu_hamburguesa

---
+++ src/main.js
@@ -0,0 +1,6 @@
+let icon = document.getElementById('icon__menu');
+let menu = document.getElementById('menu__mobile');
+
+icon.addEventListener('click', function(){
+    menu.classList.toggle('show__menu')
+})
