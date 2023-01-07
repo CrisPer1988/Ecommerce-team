65ba293f596b9ae4de22f1071856a57080968b9b
LAGAxyz <gutierrezanicamaluisalberto@hotmail.com>
2023-01-07 13:16:22 -0500

    order code

--- src/js/main.js
+++ src/js/main.js
@@ -28,32 +28,26 @@ function printProducts() {
     let html = ""
 
     productos.forEach(function({ id, name, price, stock, image }){
-        html += `
-        <div class="produc__print">
-            <div class="product__img">
-                <img src="${image}" alt="${name}" />
-                <button class="product__button button__float" id="${id}">+</button>
-            </div>
+        html += `   <div class="produc__print">
+                        <div class="product__img">
+                            <img src="${image}" alt="${name}" />
+                            <button class="product__button button__float" id="${id}">+</button>
+                        </div>
 
-            <div class="product__info">
-                <p>${name}</p>
-                <p>Precio: ${price}</p>
-                <p>Stock: ${stock}</p>
-            </div>
-
-            
-    </div>
-`;
+                        <div class="product__info">
+                            <p>${name}</p>
+                            <p>Precio: ${price}</p>
+                            <p>Stock: ${stock}</p>
+                        </div>
+                    </div>`;
     })
 
     products.innerHTML = html
 } 
 printProducts()
 
-
-
-
 //-----------------------------------------------------------------
+
 let iconMenu = document.getElementById("icon__menu");
 let menuMobile = document.getElementById("menu__mobile");
 let contentMobileMenu = document.querySelectorAll("#menu__mobile a");
