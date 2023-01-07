2152484de04c3a34cfe1a75a052f11744c74b6d8
LAGAxyz <gutierrezanicamaluisalberto@hotmail.com>
2023-01-07 09:55:22 -0500

    Barra de desplazamiento y header OK

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,8 @@
+
 let iconMenu = document.getElementById('icon__menu');
 let menuMobile = document.getElementById('menu__mobile');
+let contentMenu = document.querySelectorAll("#menu__mobile a");
+let header = document.querySelector(".header__container");
 
 function closeMenu() {
     menuMobile.classList.toggle('show__menu') 
@@ -7,9 +10,26 @@ function closeMenu() {
 
 iconMenu.addEventListener('click', closeMenu);
 
-let contentMenu = document.querySelectorAll("#menu__mobile a");
-
 for (const a of contentMenu) {
     a.addEventListener("click", closeMenu)
     
 }
+
+// FUNCIÓN con IntersectionObserver para cambiar los estilos de la barra de navegación
+(()=>{
+    let observador = document.querySelector("observador");
+
+    const options = {
+        root: null,
+        rootMargin: '25px 0px 0px 0px'
+        // threshold: 0
+    }
+
+    function callback(entries, observer){
+        console.log("funciona");
+        header.classList.toggle('transparent')
+    }
+
+    const observer = new IntersectionObserver(callback, options);
+    observer.observe(observador);
+})();
