9c8f25f04dcb5d49f8fb3e022041c6a22a941735
CrisPer1988 <norberto.cp@hotmail.com>
2023-01-07 14:38:13 -0300

    products_js

--- src/js/main.js
+++ src/js/main.js
@@ -1,35 +1,85 @@
+let productos = [
+    {
+        id: "1",
+        name: "Buzo rojo",
+        price: "14.00",
+        stock: 10,
+        image: "./src/images/featured1.png",
+    },
+    {
+        id: "2",
+        name: "Buzo negro",
+        price: "24.00",
+        stock: 15,
+        image: "./src/images/featured2.png",
+    },
+    {
+        id: "3",
+        name: "Buzo gris",
+        price: "24.00",
+        stock: 10,
+        image: "./src/images/featured3.png",
+    },
+]
 
-let iconMenu = document.getElementById('icon__menu');
-let menuMobile = document.getElementById('menu__mobile');
-let contentMenu = document.querySelectorAll("#menu__mobile a");
-let header = document.querySelector(".header__container");
+const products = document.querySelector(".products__print")
+
+function printProducts() {
+    let html = ""
+
+    productos.forEach(function({ id, name, price, stock, image }){
+        html += `
+        <div class="produc__print">
+            <div class="product__img">
+                <img src="${image}" alt="${name}" />
+                <button class="button__float">+</button>
+            </div>
+
+            <div class="product__info">
+                <p>${name}</p>
+                <p>Precio: ${price}</p>
+                <p>Stock: ${stock}</p>
+            </div>
+
+            <div class="product__button" id="${id}">
+            <button class="button__float">+</button>
+            </div>
+    </div>
+`;
+    })
+
+    products.innerHTML = html
+} 
+printProducts()
+
+
+
+
+//-----------------------------------------------------------------
+let iconMenu = document.getElementById("icon__menu");
+let menuMobile = document.getElementById("menu__mobile");
+let contentMobileMenu = document.querySelectorAll("#menu__mobile a");
 
 function closeMenu() {
-    menuMobile.classList.toggle('show__menu') 
+  menuMobile.classList.toggle("show__menu");
 }
 
-iconMenu.addEventListener('click', closeMenu);
+iconMenu.addEventListener("click", closeMenu);
 
-for (const a of contentMenu) {
-    a.addEventListener("click", closeMenu)
-    
-}
+contentMobileMenu.forEach((link) => {
+  link.addEventListener("click", closeMenu);
+});
 
-// FUNCIÓN con IntersectionObserver para cambiar los estilos de la barra de navegación
-(()=>{
-    let observador = document.querySelector("observador");
+/* Shopping Cart's code */
 
-    const options = {
-        root: null,
-        rootMargin: '25px 0px 0px 0px'
-        // threshold: 0
-    }
+const shoppingBag = document.querySelector(".shopping__bag--summary");
+const bagIcon = document.querySelector(".fa-bag-shopping");
+const closeIcon = document.querySelector(".bx-x");
 
-    function callback(entries, observer){
-        console.log("funciona");
-        header.classList.toggle('transparent')
-    }
+bagIcon.addEventListener("click", function () {
+  shoppingBag.classList.add("shopping__bag--active");
+});
 
-    const observer = new IntersectionObserver(callback, options);
-    observer.observe(observador);
-})();
+closeIcon.addEventListener("click", function () {
+  shoppingBag.classList.remove("shopping__bag--active");
+});
